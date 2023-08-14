d3da0270a9b9244be7c8d134b0ca5a0517899a1c
AlexandraTalpos <142239978+AlexandraTalpos@users.noreply.github.com>
2023-08-14 18:56:13 +0300

    Update P1.py

--- P1.py
+++ P1.py
@@ -1,29 +1,42 @@
-import csv #import modul csv implicit
-
-nume_fisier="Date.csv" #calea catre fisierul tinta
-
-#listare intreg document .csv
-with open(nume_fisier, 'r', newline='') as csvfile:
-    catalog_read = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in catalog_read:
-        print (row [0],",", row[1],",",row[2],",", row[9]) 
-        
-with open(nume_fisier, 'r', newline='') as csvfile:
-    catalog_read = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in catalog_read:
-        print (row [3],",", row[4],",",row[5],",", row[9]) 
-
-
-# creating a simple add function
-def add(a,b):
-    return a+b
+# importing the csv module
+ import csv
  
-# creating a simple odd_even function
-# to check if the number is odd or even
-def odd_even(n):
-    if n % 2 == 0:
-        print("Even")
-    else:
-        print("Odd")
+ # my data rows as dictionary objects
+ mydict =[{'skill': 'Communication', 'grade degree': '10',
+ 		'name': 'Ana', 'year': '1976'},
+ 		{'skill': 'Ledearship', 'grade degree': '7',
+ 		'name': 'Maria', 'year': '2000'},
+		{'skill': 'Empathy', 'grade degree': '9',
+ 		'name': 'Alexandra', 'year': '1988'},
+		{'skill': 'Confidence', 'grade degree': '8',
+ 		'name': 'Andreea', 'year': '1999'}]
+ 
+ 
+ # field names
+ fields = ['name', 'skill', 'year', 'grade degree']
+ 
+ # name of csv file
+filename = "P1note.csv" 
+ # writing to csv file
+ with open(filename, 'w') as csvfile:
+	# creating a csv dict writer object
+	writer = csv.DictWriter(csvfile, fieldnames = fields)
+	
+	# writing headers (field names)
+ 	writer.writeheader()	
+	# writing data rows
+	writer.writerows(mydict)
+	
 
+# name of csv file
+filename = "P1note.csv"
 
+# writing to csv file
+with open(filename, 'w') as csvfile:
+	# creating a csv dict writer object
+	writer = csv.DictWriter(csvfile, fieldnames = fields)
+
+	# writing headers (field names)
+	writer.writeheader()
+
+	# writing data rows
