6f7543f86edd81483e95124d986212d55fe8dbe5
AlexandraTalpos <142239978+AlexandraTalpos@users.noreply.github.com>
2023-08-14 19:47:50 +0300

    Code1.py

--- P1.py
+++ P1.py
@@ -1,9 +1,37 @@
-import csv #import modul csv implicit
+import csv
 
-nume_fisier="P1note.csv" #calea catre fisierul tinta
+# Deschideți fișierul CSV pentru citire
+with open('Note.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
 
-#listare intreg document .csv
-with open(nume_fisier, 'r', newline='') as csvfile:
-    catalog_read = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in catalog_read:
-        print (row [0],",", row[1],",",row[2],",", row[3])
+    # Creați o listă pentru a stoca elevii și notele lor
+    students = []
+
+    # Parcurgeți rândurile din fișier
+    for row in reader:
+        # Adăugați elevul și notele sale la listă
+        students.append({
+            'name': row[0],
+            'math': int(row[1]),
+            'romanian': int(row[2])
+        })
+
+    # Cereți utilizatorului ce elev dorește să aleagă
+    name = input('Ce elev doriți să alegeți? ')
+
+    # Găsiți elevul în listă
+    student = next(student for student in students if student['name'] == name)
+
+    # Cereți utilizatorului ce operațiune dorește să efectueze
+    operation = input('Ce operațiune doriți să efectuați? (adunare, scădere, medie aritmetică) ')
+
+    # Efectuați operația dorită
+    if operation == 'adunare':
+        result = student['math'] + student['romanian']
+    elif operation == 'scădere':
+        result = student['math'] - student['romanian']
+    else:
+        result = (student['math'] + student['romanian']) / 2
+
+    # Imprimați rezultatul
+    print('Rezultatul este:', result)
